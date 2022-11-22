fcaedb1ba70722e30552e41ea0b679c468ca39d1
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2022-11-22 14:59:20 -0300

    adiciona rota de login

--- src/main/java/web/oficina/controller/IndexController.java
+++ src/main/java/web/oficina/controller/IndexController.java
@@ -1,14 +1,25 @@
 package web.oficina.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
 	@GetMapping(value = {"/", "/index.html"} )
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		logger.trace("Entrou em login");
+		logger.trace("Encaminhando para a view login");
+		return "login";
 	}
 	
 }
